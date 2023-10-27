081ef951f87844ca0a9d421554fd68a0c85128d6
Hassaaan Ahmed <hassanhere04@gmail.com>
2023-10-28 03:07:33 +0500

    comit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
     projectName: 'my-website', // Usually your repo name.
     deploymentBranch: 'deployment',
     trailingSlash: false,
-    onBrokenLinks: 'throw',
+    onBrokenLinks: 'warn',
     onBrokenMarkdownLinks: 'warn',
 
     // Even if you don't use internalization, you can use this field to set useful
